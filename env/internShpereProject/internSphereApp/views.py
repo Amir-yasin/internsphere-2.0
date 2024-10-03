df802ab4f4e19467564d62b5868418580c2b87cc
Amir Yasin <amiryasin3262@gmail.com>
2024-10-03 07:47:56 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   env/internShpereProject/internSphereApp/__pycache__/urls.cpython-311.pyc
    	modified:   env/internShpereProject/internSphereApp/__pycache__/views.cpython-311.pyc
    	new file:   env/internShpereProject/internSphereApp/templates/company_pages/company_dashboard.html
    	renamed:    env/internShpereProject/internSphereApp/templates/company_pages/companies.html -> env/internShpereProject/internSphereApp/templates/company_pages/post_internship.html
    	modified:   env/internShpereProject/internSphereApp/urls.py
    	modified:   env/internShpereProject/internSphereApp/views.py
    	new file:   env/internShpereProject/static/css/company_dashboard.css
    
    Changes not staged for commit:
    	modified:   env/internShpereProject/internSphereApp/views.py

--- env/internShpereProject/internSphereApp/views.py
+++ env/internShpereProject/internSphereApp/views.py
@@ -1,30 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+        # main pages
 def home(request):
     return render(request, 'main_pages/index.html', {'current_page': 'home'})
 def about(request):
     return render(request, 'main_pages/About.html', {'current_page': 'about'})
-def companies(request):
-    return render(request, 'company_pages/companies.html', {'current_page': 'companies'})
 def contact(request):
     return render(request, 'main_pages/contact.html', {'current_page': 'contact'})
-def Graduate_students(request):
-    return render(request, 'student_pages/Graduate_students.html', {'current_page': 'Graduate_students'})
-def intern_opportunities(request):
-    return render(request, 'student_pages/intern_opportunities.html', {'current_page': 'intern_opportunities'})
-def Internships(request):
-    return render(request, 'student_pages/Internships.html', {'current_page': 'Internships'})
 def login(request):
     return render(request, 'main_pages/login.html', {'current_page': 'login'})
 def register(request):
     return render(request, 'main_pages/register.html', {'current_page': 'register'})
 def services(request):
     return render(request, 'main_pages/services.html', {'current_page': 'services'})
+
+
+
+# student pages
+
+
+def Graduate_students(request):
+    return render(request, 'student_pages/Graduate_students.html', {'current_page': 'Graduate_students'})
+def intern_opportunities(request):
+    return render(request, 'student_pages/intern_opportunities.html', {'current_page': 'intern_opportunities'})
+def Internships(request):
+    return render(request, 'student_pages/Internships.html', {'current_page': 'Internships'})
+
 def student_profile(request):
     return render(request, 'student_pages/student_profile.html', {'current_page': 'student_profile'})
-# def universities(request):
-#     return render(request, 'universities.html', {'current_page': 'universities'})
+def company_dashboard(request):
+    return render(request, 'company_pages/company_dashboard.html', {'current_page': 'company_dashboard'})
 def bi_weekly_report(request):
     return render(request, 'student_pages/bi_weekly_report.html', {'current_page': 'bi_weekly_report'})
 def company_profile(request):
@@ -35,6 +41,7 @@ def applications(request):
     return render(request, 'student_pages/applications.html', {'current_page': 'applications'})
 def stud_notification(request):
     return render(request, 'student_pages/stud_notification.html', {'current_page': 'stud_notification'})
+
 # def stud_notification(request):
 #     return render(request, 'stud_notification.html', {'current_page': 'stud_notification'})
 # def stud_notification(request):
@@ -42,4 +49,4 @@ def stud_notification(request):
 # def stud_notification(request):
 #     return render(request, 'stud_notification.html', {'current_page': 'stud_notification'})
 # def stud_notification(request):
-#     return render(request, 'stud_notification.html', {'current_page': 'stud_notification'})
+#     return render(request, 'stud_notification.html', {'current_page': 'stud_notification'
